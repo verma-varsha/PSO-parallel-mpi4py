dd906a64fd25a037364725f465688ccebf88e17b
verma_varsha <varsha.verma.eee15@itbhu.ac.in>
2018-11-24 17:06:30 +0530

    modified objective function

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # PSO-parallel-mpi4py
 This repository contains the code for parallelization of Particle Swarm Optimization Algorithm on multiple nodes using MPI (via mpi4py python package) using a master-slave model of implementation.
 
+The objective function that has been minimized by the Particle Swarm Optimization Algorithm is the Alpine 1 Function which has been made artificially expensive by introducing a pause of 0.1 seconds.
 
 ## Setup
 There are two types of nodes in master-slave model of parallelization. There is only one master node in the system and the rest of the other processes are the slave nodes. Therefore, there are two kinds of instructions, one running on the master node and the other running on the slave nodes. 
-The code in `parPSOmaster.py` is to be loaded onto the master node and the one in `parPSOslave.py` is to be loaded onto the slave nodes. Note that the code on all the nodes should be saved at the same location with the same name. For instance, I saved it as `parPSO.py` in the Documents directory on all the nodes. 
+When rank=0, instructions for master node are executed and nodes with all the other ranks have slave instructions executed as can be seen in the code. Note that the code file on all the nodes should be saved at the same location with the same name. For instance, I saved it as `parPSO.py` in the Documents directory on all the nodes. 
 
 ## Running Instructions
 
--- parPSO.py
+++ parPSO.py
@@ -1,6 +1,16 @@
 import random
 from mpi4py import MPI
 from collections import deque
+import time
+import math
+
+# ARTIFICIALLY EXPENSIVE ALPINE OBJECTIVE FUNCTION
+def costFunction(x):
+	a = 0
+	time.sleep(0.01) # to make this artificially expensive
+	for i in x:
+		a+= abs((math.sin(i)*i) + (0.1*i))
+	return a
 
 class Particle:
 	def __init__(self, x0):
@@ -12,7 +22,7 @@ class Particle:
 
 		for i in range(0, num_dimensions):
 			self.velocity.append(random.uniform(-1, 1))
-			self.position.append(x0[i])
+			self.position.append(random.uniform(bounds[0], bounds[1]))
 
 
 
@@ -34,32 +44,26 @@ class Particle:
 		for i in range(0, num_dimensions):
 			self.position[i]+=self.velocity[i]
 
-			if self.position[i]<bounds[i][0]:
-				self.position[i]=bounds[i][0]
+			if self.position[i]<bounds[0]:
+				self.position[i]=bounds[0]
 
-			if self.position[i]>bounds[i][1]:
-				self.position[i]=bounds[i][1]
+			if self.position[i]>bounds[1]:
+				self.position[i]=bounds[1]
 
 
 class PSO():
-	def __init__(self, x0, bounds, num_particles, num_iter):
+	def __init__(self, num_d, bounds, num_particles, num_iter):
 		global num_dimensions
-		num_dimensions=len(x0)
+		num_dimensions=num_d
 
 		best_cost_g=float('inf')
 		best_pos_g=[]
 
 		swarm=[]
 		for i in range(0, num_particles):
-			swarm.append(Particle(x0))
+			swarm.append(Particle(bounds))
 
-		
 		for i in range(num_iter):
-
-
-			#######################################################################
-			#      PARALLEL COST FUNCTION EVALUATION FOR POPULATION BEGINS        #
-			#######################################################################
 
 			evalQueue = deque(range(num_particles))
 
@@ -97,19 +101,14 @@ class PSO():
 				if idle==size-1:
 					break
 
-			#######################################################################
-			#      PARALLEL COST FUNCTION EVALUATION FOR POPULATION ENDS        #
-			#######################################################################
 
 			for j in range(0, num_particles):
 				swarm[j].update_velocity(best_pos_g)
 				swarm[j].update_position(bounds)
 
-		# Sending done signal to all slave nodes
+		
 		for k in range(1,size):
-			comm.send(0, dest=k, tag=200)	
-
-		# Printing the final results to the console
+			comm.send(0, dest=k, tag=200)   
 		print 'Best position : '
 		print best_pos_g
 		print 'Best cost : '
@@ -120,6 +119,21 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 status = MPI.Status()
-x0=[2,2]
-bounds=[(-5, 5), (-5, 5)]
-PSO(x0, bounds, num_particles=15, num_iter=100)
+
+if rank==0:
+	start_time = time.time()
+	num_d=20
+	bounds=(-10,10)
+	PSO(num_d, bounds, num_particles=15, num_iter=400)
+	print("time taken:")
+	print(time.time()-start_time)
+else:
+	while(1):
+		obj_recv = comm.recv(source=0, tag=MPI.ANY_TAG, status=status)
+		tag = status.Get_tag()
+		if tag == 200:
+			break
+
+		f = costFunction(obj_recv[1])
+		obj_sent = (obj_recv[0], f)
+		comm.send(obj_sent, dest=0)
